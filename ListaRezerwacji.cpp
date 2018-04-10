9f25de0dffb8b7f1149baa02fc17211f85c4de6c
adleea <adela.zdrzalka@gmail.com>
2018-04-10 16:54:17 +0200

    Update ListaRezerwacji.cpp

--- ListaRezerwacji.cpp
+++ ListaRezerwacji.cpp
@@ -1,9 +1,8 @@
-#ListaRezerwacji
-
 #include <iostream>
-#include<vector>
-#include<cstring>
+#include <vector>
+#include <cstdlib>
 #include "Rezerwacja.cpp"
+
 using namespace std;
 
 //Tworzenie klasy
@@ -12,42 +11,35 @@ class ListaRezerwacji{
 
 private:
         int liczbaRezerwacji;
-        vector <Rezerwacja> lista;
+        vector <Rezerwacja> lista_r;
 public:
         ListaRezerwacji(){
+
             liczbaRezerwacji = 0;
         };
-        void anuluj_rezerwacje(int ID);
-        pokaz1rezerwacje();
+
+        void anuluj_rezerwacje(int ID){
+
+            for(int i=0; i<lista_r.size(); i++)
+            {
+                if(lista_r.at(i).pokazID() == ID)
+                    lista_r.erase(lista_r.begin()+i);
+            }
+        }
+       // pokaz1rezerwacje();
+
         vector <Rezerwacja>  pokaz_rezerwacje()
         {
-            return lista;
+            return lista_r;
         };
-        void zrob_rezerwacje(int ID);
+
+        void zrob_rezerwacje(int ID){
+
+            for(int i=0; i<katalog.size(); i++)
+            {
+                if(katalog.at(i).pokazID() == ID)
+                    lista_r.push_back(Rezerwacja);
+            }
+        }
+
 };
-
-//Konstruktor
-
-ListaRezerwacji::ListaRezerwacji(int liczbaRezerwacji) {
-    this->liczbaRezerwacji= liczbaRezerwacji;
-}
-
-
-void ListaRezerwacji:: anuluj_rezerwacje(int ID){
-
-    for(int i=0; i<lista.size(); i++)
-    {
-        if(lista.at(i).pokazID() == ID)
-            lista.erase(lista.begin()+i);
-    }
-
-}
-void ListaRezerwacji::pokaz1rezerwacje(int ID) {
-    strcpy(int id);
-    cout<<ID.rezerwacja<<endl;  //??
-}
-void ListaRezerwacji::zrob_rezerwacje(int ID){
-        lista.push_back(rezerwacja);
-}
-
-
